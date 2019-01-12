8ca177804c12ab76c77ff6be509bdf221852f29a
Luke Chen <lukekchenkk@gmail.com>
2019-01-12 00:33:42 -0500

    Set up abstract transform methods, added config and joystick

---
+++ DeepSpaceRobot/src/main/java/frc/robot/Abstract/Transform.java
@@ -0,0 +1,9 @@
+public abstract class Transform {
+    
+    public abstract void reduceSens();
+
+    public abstract void increaseSens();
+
+    public abstract void returnNorm();
+
+}
---
+++ DeepSpaceRobot/src/main/java/frc/robot/Config.java
@@ -0,0 +1,8 @@
+public class Config {
+
+    public class Input {
+        public static final int LEFT_JOYOSTICK_PORT = 0;
+        public static final int LEFT_JOYOSTICK_PORT = 1;
+    }
+
+}
