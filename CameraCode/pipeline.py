195ceb76f38d328678581769d0279b5ced844576
RishabRao <rishabpaws@gmail.com>
2019-01-15 20:35:41 -0500

    updated pipeline

--- CameraCode/pipeline.py
+++ CameraCode/pipeline.py
@@ -12,25 +12,11 @@ class FilterLines:
         """initializes all values to presets or None if need to be set
         """
 
-        self.__hsl_threshold_hue = [87.41006998384157, 100.54329175956181]
-        self.__hsl_threshold_saturation = [57.32913669064748, 255.0]
-        self.__hsl_threshold_luminance = [0.0, 255.0]
+        self.__hsl_threshold_hue = [69.60431654676259, 99.01528013582343]
+        self.__hsl_threshold_saturation = [155.93525179856115, 255.0]
+        self.__hsl_threshold_luminance = [91.72661870503596, 155.42444821731752]
 
         self.hsl_threshold_output = None
-
-        self.__mask_mask = self.hsl_threshold_output
-
-        self.mask_output = None
-
-        self.__find_lines_input = self.mask_output
-
-        self.find_lines_output = None
-
-        self.__filter_lines_lines = self.find_lines_output
-        self.__filter_lines_min_length = 16.0
-        self.__filter_lines_angle = [0.0, 360.0]
-
-        self.filter_lines_output = None
 
         self.__find_contours_input = self.hsl_threshold_output
         self.__find_contours_external_only = False
@@ -38,19 +24,33 @@ class FilterLines:
         self.find_contours_output = None
 
         self.__filter_contours_contours = self.find_contours_output
-        self.__filter_contours_min_area = 10.0
-        self.__filter_contours_min_perimeter = 0
-        self.__filter_contours_min_width = 0
-        self.__filter_contours_max_width = 1000
-        self.__filter_contours_min_height = 0
-        self.__filter_contours_max_height = 1000
+        self.__filter_contours_min_area = 6.0
+        self.__filter_contours_min_perimeter = 5.0
+        self.__filter_contours_min_width = 4.0
+        self.__filter_contours_max_width = 1000.0
+        self.__filter_contours_min_height = 3.0
+        self.__filter_contours_max_height = 1000.0
         self.__filter_contours_solidity = [0, 100]
-        self.__filter_contours_max_vertices = 1000000
-        self.__filter_contours_min_vertices = 0
-        self.__filter_contours_min_ratio = 0
-        self.__filter_contours_max_ratio = 1000
+        self.__filter_contours_max_vertices = 1000000.0
+        self.__filter_contours_min_vertices = 4.0
+        self.__filter_contours_min_ratio = 0.0
+        self.__filter_contours_max_ratio = 1000.0
 
         self.filter_contours_output = None
+
+
+
+        self.get_mat_info_size = None
+
+        self.get_mat_info_empty = None
+
+        self.get_mat_info_channels = None
+
+        self.get_mat_info_cols = None
+
+        self.get_mat_info_rows = None
+
+        self.get_mat_info_high_value = None
 
 
     def process(self, source0):
@@ -61,19 +61,6 @@ class FilterLines:
         self.__hsl_threshold_input = source0
         (self.hsl_threshold_output) = self.__hsl_threshold(self.__hsl_threshold_input, self.__hsl_threshold_hue, self.__hsl_threshold_saturation, self.__hsl_threshold_luminance)
 
-        # Step Mask0:
-        self.__mask_input = source0
-        self.__mask_mask = self.hsl_threshold_output
-        (self.mask_output) = self.__mask(self.__mask_input, self.__mask_mask)
-
-        # Step Find_Lines0:
-        self.__find_lines_input = self.mask_output
-        (self.find_lines_output) = self.__find_lines(self.__find_lines_input)
-
-        # Step Filter_Lines0:
-        self.__filter_lines_lines = self.find_lines_output
-        (self.filter_lines_output) = self.__filter_lines(self.__filter_lines_lines, self.__filter_lines_min_length, self.__filter_lines_angle)
-
         # Step Find_Contours0:
         self.__find_contours_input = self.hsl_threshold_output
         (self.find_contours_output) = self.__find_contours(self.__find_contours_input, self.__find_contours_external_only)
@@ -81,6 +68,10 @@ class FilterLines:
         # Step Filter_Contours0:
         self.__filter_contours_contours = self.find_contours_output
         (self.filter_contours_output) = self.__filter_contours(self.__filter_contours_contours, self.__filter_contours_min_area, self.__filter_contours_min_perimeter, self.__filter_contours_min_width, self.__filter_contours_max_width, self.__filter_contours_min_height, self.__filter_contours_max_height, self.__filter_contours_solidity, self.__filter_contours_max_vertices, self.__filter_contours_min_vertices, self.__filter_contours_min_ratio, self.__filter_contours_max_ratio)
+
+        # Step Get_Mat_Info0:
+        self.__get_mat_info_input = source0
+        (self.get_mat_info_size,self.get_mat_info_empty,self.get_mat_info_channels,self.get_mat_info_cols,self.get_mat_info_rows,self.get_mat_info_high_value) = self.__get_mat_info(self.__get_mat_info_input)
 
 
     @staticmethod
@@ -96,70 +87,6 @@ class FilterLines:
         """
         out = cv2.cvtColor(input, cv2.COLOR_BGR2HLS)
         return cv2.inRange(out, (hue[0], lum[0], sat[0]),  (hue[1], lum[1], sat[1]))
-
-    @staticmethod
-    def __mask(input, mask):
-        """Filter out an area of an image using a binary mask.
-        Args:
-            input: A three channel numpy.ndarray.
-            mask: A black and white numpy.ndarray.
-        Returns:
-            A three channel numpy.ndarray.
-        """
-        return cv2.bitwise_and(input, input, mask=mask)
-
-    class Line:
-
-        def __init__(self, x1, y1, x2, y2):
-            self.x1 = x1
-            self.y1 = y1
-            self.x2 = x2
-            self.y2 = y2
-
-        def length(self):
-            return numpy.sqrt(pow(self.x2 - self.x1, 2) + pow(self.y2 - self.y1, 2))
-
-        def angle(self):
-            return math.degrees(math.atan2(self.y2 - self.y1, self.x2 - self.x1))
-    @staticmethod
-    def __find_lines(input):
-        """Finds all line segments in an image.
-        Args:
-            input: A numpy.ndarray.
-        Returns:
-            A filtered list of Lines.
-        """
-        detector = cv2.createLineSegmentDetector()
-        if (len(input.shape) == 2 or input.shape[2] == 1):
-            lines = detector.detect(input)
-        else:
-            tmp = cv2.cvtColor(input, cv2.COLOR_BGR2GRAY)
-            lines = detector.detect(tmp)
-        output = []
-        if len(lines) != 0:
-            for i in range(1, len(lines[0])):
-                tmp = FilterLines.Line(lines[0][i, 0][0], lines[0][i, 0][1],
-                                lines[0][i, 0][2], lines[0][i, 0][3])
-                output.append(tmp)
-        return output
-
-    @staticmethod
-    def __filter_lines(inputs, min_length, angle):
-        """Filters out lines that do not meet certain criteria.
-        Args:
-            inputs: A list of Lines.
-            min_Lenght: The minimum lenght that will be kept.
-            angle: The minimum and maximum angles in degrees as a list of two numbers.
-        Returns:
-            A filtered list of Lines.
-        """
-        outputs = []
-        for line in inputs:
-            if (line.length() > min_length):
-                if ((line.angle() >= angle[0] and line.angle() <= angle[1]) or
-                        (line.angle() + 180.0 >= angle[0] and line.angle() + 180.0 <= angle[1])):
-                    outputs.append(line)
-        return outputs
 
     @staticmethod
     def __find_contours(input, external_only):
@@ -222,6 +149,26 @@ class FilterLines:
                 continue
             output.append(contour)
         return output
+
+    @staticmethod
+    def __get_mat_info(src):
+        """Gets information about given Mat.
+        Args:
+            src: A numpy.ndarray.
+        Returns:
+            The size of the mat as a list of two numbers.
+            A boolean that is true if the mat is empty.
+            The number of the channels in the mat.
+            The number of columns.
+            The number of rows.
+            The highest value in the mat.
+        """
+        cols, rows, channels = src.shape
+        empty = (src.size==0)
+        lowest_value, highest_value = cv2.minMaxLoc(src if (channels == 1)
+                            else cv2.cvtColor(src, cv2.COLOR_BGR2GRAY))
+        mat_size = (rows, cols)
+        return mat_size, empty, channels, cols, rows, highest_value
 
 
 
