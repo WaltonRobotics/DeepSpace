04aed6e3cc5e76eb69194538451c57fd75e5e74c
Luke Chen <lukekchenkk@gmail.com>
2019-01-19 13:48:55 -0500

    fixed variable issue

--- DeepSpaceRobot/src/main/java/frc/robot/command/teleop/DriverTakeOver.java
+++ DeepSpaceRobot/src/main/java/frc/robot/command/teleop/DriverTakeOver.java
@@ -1,5 +1,5 @@
 /*----------------------------------------------------------------------------*/
-/* Copyright (c) 2018 LUKELMAOXDRAWR. All Rights Reserved.                             */
+/* Copyright (c) 2018 LUKELMAOXDRAWR. All Rights Reserved.                    */
 /* Open Source Software - may be modified and shared by FRC teams. The code   */
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
@@ -15,30 +15,31 @@ import edu.wpi.first.wpilibj.command.Command;
 public class DriverTakeOver extends Command {
 
     double[] noActivity = {0, 0};
+    double[] controllerAct = new double[2];
     double[] activity = new double[2];
     Log eventLog = new Log();
     boolean takeOver;
-    
+
     //needs to be called somewhere
     public DriverTakeOver() {
         while (isFinished() == false) {
             eventLog.leftJoystickActivity = leftJoystick.getY();
             eventLog.rightJoystickActivity = rightJoystick.getY();
-            double[] controllerAct = {eventLog.leftJoystickActivity, eventLog.rightJoystickActivity};
+            controllerAct = new double[]{eventLog.leftJoystickActivity, eventLog.rightJoystickActivity};
             activity = controllerAct;
-        }   
+        }
     }
-    
+
     public void isTakingOver() {
         if(activity == noActivity) {
             takeOver = false;
         }
-        takeOver = true;
-    }
+            takeOver = true;
+        }
 
-	    @Override
-	    protected boolean isFinished() {
-		    return false;
+	@Override
+	protected boolean isFinished() {
+		return false;
 	}
     
     }
