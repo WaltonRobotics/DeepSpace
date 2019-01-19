cf0f41c9ee7a8faa8c43f22dd65fb19863101732
Luke Chen <lukekchenkk@gmail.com>
2019-01-19 13:35:10 -0500

    Update DriverTakeOver.java

--- DeepSpaceRobot/src/main/java/frc/robot/command/teleop/DriverTakeOver.java
+++ DeepSpaceRobot/src/main/java/frc/robot/command/teleop/DriverTakeOver.java
@@ -1,5 +1,5 @@
 /*----------------------------------------------------------------------------*/
-/* Copyright (c) 2018 LUKELMAOXDRAWR. All Rights Reserved.                             */
+/* Copyright (c) 2018 LUKELMAOXDRAWR. All Rights Reserved.                    */
 /* Open Source Software - may be modified and shared by FRC teams. The code   */
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
@@ -34,8 +34,8 @@ public class DriverTakeOver extends Command {
         if(activity == noActivity) {
             takeOver = false;
         }
-        takeOver = true;
-    }
+            takeOver = true;
+        }
 
 	@Override
 	protected boolean isFinished() {
