3a81c3103b6af75f23a628a8f1bfe1c1ff81b6c1
NamelessSuperCoder <Marius.juston@hotmail.fr>
2019-01-20 09:38:24 -0500

    made drivetrain use library drivetrain
    drivetrain now extends AbstractDrivetrain

--- DeepSpaceRobot/src/main/java/frc/robot/subsystem/Drivetrain.java
+++ DeepSpaceRobot/src/main/java/frc/robot/subsystem/Drivetrain.java
@@ -10,20 +10,97 @@ package frc.robot.subsystem;
 import static frc.robot.RobotMap.leftWheel;
 import static frc.robot.RobotMap.rightWheel;
 
-import edu.wpi.first.wpilibj.command.Subsystem;
 import frc.robot.RobotMap;
 import frc.robot.command.teleop.Drive;
+import org.waltonrobotics.AbstractDrivetrain;
+import org.waltonrobotics.controller.RobotPair;
 
 /**
  * Add your docs here.
  */
-public class Drivetrain extends Subsystem {
+public class Drivetrain extends AbstractDrivetrain {
   // Put methods for controlling this subsystem
   // here. Call these from Commands.
 
 
   public Drivetrain() {
 
+  }
+
+  @Override
+  public RobotPair getWheelPositions() {
+    return null;
+  }
+
+  @Override
+  public double getRobotWidth() {
+    return 0;
+  }
+
+  @Override
+  public void reset() {
+
+  }
+
+  @Override
+  public void setSpeeds(double leftYJoystick, double rightYJoystick) {
+    leftWheel.set(leftYJoystick);
+    rightWheel.set(rightYJoystick);
+  }
+
+  @Override
+  public void setEncoderDistancePerPulse() {
+
+  }
+
+  @Override
+  public double getKV() {
+    return 0;
+  }
+
+  @Override
+  public double getKAcc() {
+    return 0;
+  }
+
+  @Override
+  public double getKK() {
+    return 0;
+  }
+
+  @Override
+  public double getKS() {
+    return 0;
+  }
+
+  @Override
+  public double getKAng() {
+    return 0;
+  }
+
+  @Override
+  public double getKL() {
+    return 0;
+  }
+
+  @Override
+  public double getILag() {
+    return 0;
+  }
+
+  @Override
+  public double getIAng() {
+    return 0;
+  }
+
+  @Override
+  public double getMaxVelocity() {
+    return 0;
+  }
+
+  @Override
+  public double getMaxAcceleration() {
+    return 0;
   }
 
 
@@ -33,10 +110,6 @@ public class Drivetrain extends Subsystem {
     setDefaultCommand(new Drive());
   }
 
-  public void setSpeed(double leftYJoystick, double rightYJoystick) {
-    leftWheel.set(leftYJoystick);
-    rightWheel.set(rightYJoystick);
-  }
 
   public void shiftUp() {
     if (RobotMap.shifter.get()) {
