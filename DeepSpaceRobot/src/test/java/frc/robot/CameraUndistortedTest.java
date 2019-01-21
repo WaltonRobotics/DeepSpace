eb5845159892bf33322f07d50393ffef00cd026b
NamelessSuperCoder <Marius.juston@hotmail.fr>
2019-01-21 11:08:24 -0500

    modulized mat creation

--- DeepSpaceRobot/src/test/java/frc/robot/CameraUndistortedTest.java
+++ DeepSpaceRobot/src/test/java/frc/robot/CameraUndistortedTest.java
@@ -11,21 +11,22 @@ import org.junit.Before;
 import org.junit.Test;
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
+import org.opencv.core.Size;
 
 public class CameraUndistortedTest {
 
-  public static double[][] k = {{540.6169741181128, 0.0, 979.9714037950379},
+  private static double[][] k = {{540.6169741181128, 0.0, 979.9714037950379},
       {0.0, 540.4923723031342, 603.6179611901534}, {0.0, 0.0, 1.0}};
-  public static double[][] scaled = {{264.74866142, 0., 628.46350396},
+  private static double[][] scaled = {{264.74866142, 0., 628.46350396},
       {0., 264.68764195, 375.72956697},
       {0., 0., 1.}};
-  public static double[][] d = {{-0.04581054920752462},
+  private static double[][] d = {{-0.04581054920752462},
       {0.00580921941468853}, {-0.0037112166109776372}, {-0.00015164559682399615}};
-  public static double[][] newKK = {{360.41131608, 0., 653.3142692},
+  private static double[][] newKK = {{360.41131608, 0., 653.3142692},
       {0., 360.3282482, 402.41197413},
       {0., 0., 1.}};
 
-  public static void loadNativeLibraries() {
+  private static void loadNativeLibraries() {
     Path path = Paths.get(RuntimeLoader.getDefaultExtractionRoot(), RuntimeDetector.getPlatformPath());
 
     File file = path.toFile();
@@ -36,7 +37,7 @@ public class CameraUndistortedTest {
     }
   }
 
-  public static void fillMat(Mat mat, double[][] data) {
+  private static void fillMat(Mat mat, double[][] data) {
     mat.create(data.length, data[0].length, CvType.CV_64F);
     for (int i = 0; i < data.length; i++) {
       mat.put(i, 0, data[i]);
@@ -48,7 +49,7 @@ public class CameraUndistortedTest {
     loadNativeLibraries();
   }
 
-  public double[][] getArray(Mat matrix) {
+  private double[][] getArray(Mat matrix) {
     double[][] data = new double[matrix.rows()][matrix.cols()];
 
     for (int r = 0; r < matrix.rows(); r++) {
@@ -60,54 +61,87 @@ public class CameraUndistortedTest {
     return data;
   }
 
+  private Mat getKMat() {
+    Mat mat = new Mat();
+    fillMat(mat, CameraUndistortedTest.k);
+    return mat;
+  }
+
   @Test
   public void kMatTest() {
 
-    Mat k = new Mat();
-    fillMat(k, CameraUndistortedTest.k);
-    double[][] kData = getArray(k);
+    Mat mat = getKMat();
+    double[][] kData = getArray(mat);
+
+    System.out.println(mat.dump());
 
     Assert.assertArrayEquals(kData, CameraUndistortedTest.k);
+  }
+
+
+  public Mat getDMat() {
+    Mat mat = new Mat();
+    fillMat(mat, CameraUndistortedTest.d);
+    return mat;
   }
 
   @Test
   public void dMatTest() {
 
-    Mat d = new Mat();
-    fillMat(d, CameraUndistortedTest.d);
-    double[][] kData = getArray(d);
+    Mat mat = getDMat();
+    double[][] kData = getArray(mat);
+
+    System.out.println(mat.dump());
 
     Assert.assertArrayEquals(kData, CameraUndistortedTest.d);
+  }
+
+  public Mat getScaledKMat() {
+    Mat mat = new Mat();
+    fillMat(mat, CameraUndistortedTest.scaled);
+    return mat;
   }
 
   @Test
   public void scaledKMatTest() {
 
-    Mat scaled = new Mat();
-    fillMat(scaled, CameraUndistortedTest.scaled);
-    double[][] kData = getArray(scaled);
+    Mat mat = getScaledKMat();
+    double[][] kData = getArray(mat);
+
+    System.out.println(mat.dump());
 
     Assert.assertArrayEquals(kData, CameraUndistortedTest.scaled);
+  }
+
+  public Mat getNewKMat() {
+    Mat mat = new Mat();
+    fillMat(mat, CameraUndistortedTest.newKK);
+    return mat;
   }
 
   @Test
   public void newKMatTest() {
 
-    Mat scaled = new Mat();
-    fillMat(scaled, CameraUndistortedTest.newKK);
-    double[][] kData = getArray(scaled);
+    Mat mat = getNewKMat();
+    double[][] kData = getArray(mat);
 
+    System.out.println(mat.dump());
     Assert.assertArrayEquals(kData, newKK);
+  }
+
+  Mat getRMat() {
+    return Mat.eye(3, 3, CvType.CV_64F);
   }
 
   @Test
   public void rMatTest() {
+    Mat mat = getRMat();
 
-    Mat R = Mat.eye(3, 3, CvType.CV_64F);
-
-    double[][] kData = getArray(R);
+    double[][] kData = getArray(mat);
 
     double[][] answer = {{1, 0, 0}, {0, 1, 0}, {0, 0, 1}};
+
+    System.out.println(mat.dump());
     Assert.assertArrayEquals(kData, answer);
   }
 
