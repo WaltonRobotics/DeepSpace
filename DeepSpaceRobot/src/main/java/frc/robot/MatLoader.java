60eddabd01195504284f6b6f22c99d920a44c06c
NamelessSuperCoder <Marius.juston@hotmail.fr>
2019-01-21 14:48:32 -0500

    added mat xml loader

---
+++ DeepSpaceRobot/src/main/java/frc/robot/MatLoader.java
@@ -0,0 +1,67 @@
+package frc.robot;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+public class MatLoader {
+
+  public final Document doc;
+
+  public MatLoader(String fileName) throws IOException, SAXException, ParserConfigurationException {
+    File fXmlFile = new File("fisheye_undistorted.xml");
+
+    DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+    DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+    doc = dBuilder.parse(fXmlFile);
+
+    //optional, but recommended
+    //read this - http://stackoverflow.com/questions/13786607/normalization-in-dom-parsing-with-java-how-does-it-work
+    doc.getDocumentElement().normalize();
+
+  }
+
+  public static Mat createMat(int rows, int cols, double[] data) {
+    Mat mat = new Mat(rows, cols, CvType.CV_64F);
+
+    int i = 0;
+    for (int r = 0; r < rows; r++) {
+      for (int c = 0; c < cols; c++) {
+        mat.put(r, c, data[i++]);
+      }
+    }
+    return mat;
+  }
+
+
+
+  public Mat getMat(String matName) {
+    NodeList nList = doc.getElementsByTagName(matName);
+
+    Node nNode = nList.item(0);
+
+    Mat mat = null;
+    if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+      Element eElement = (Element) nNode;
+
+      int rows = Integer.parseInt(eElement.getElementsByTagName("rows").item(0).getTextContent());
+      int cols = Integer.parseInt(eElement.getElementsByTagName("cols").item(0).getTextContent());
+      String dataString = eElement.getElementsByTagName("data").item(0).getTextContent();
+      double[] data = Arrays.stream(dataString.trim().split("\\s+")).mapToDouble(Double::parseDouble).toArray();
+
+      mat = createMat(rows, cols, data);
+    }
+
+    return mat;
+  }
+}
