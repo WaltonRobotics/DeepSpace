a549bdbe9620145810f70a9a0b4a2bee84c1bc15
AndrewLi8779 <42279294+AndrewLi8779@users.noreply.github.com>
2019-01-22 19:55:49 -0500

    copy pasted marius'  loadnativelibraries method

--- DeepSpaceRobot/src/test/java/frc/robot/MotorTest.java
+++ DeepSpaceRobot/src/test/java/frc/robot/MotorTest.java
@@ -1,15 +1,36 @@
 package frc.robot;
 
+import edu.wpi.first.wpiutil.RuntimeDetector;
+import edu.wpi.first.wpiutil.RuntimeLoader;
 import frc.robot.Command.teleop.Drive;
 import frc.robot.Subsystem.Drivetrain;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Objects;
+
 import static frc.robot.Robot.drivetrain;
 
 public class MotorTest
 {
+    private static void loadNativeLibraries() {
+        Path path = Paths.get(RuntimeLoader.getDefaultExtractionRoot(), RuntimeDetector.getPlatformPath());
+
+        File file = path.toFile();
+
+        for (File nativeLibrary : Objects.requireNonNull(file.listFiles())) {
+            System.out.printf("Loading: %s from %s%n", nativeLibrary.getName(), nativeLibrary.getAbsolutePath());
+            System.load(nativeLibrary.getAbsolutePath());
+        }
+    }
+    @Before
+    public void init(){
+        loadNativeLibraries();
+    }
     @Test
     public void move()
     {
