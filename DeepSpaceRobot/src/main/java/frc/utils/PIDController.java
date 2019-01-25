7cb5beadf3d017de32bbe545ad1821a1c1b79406
vmalepati1 <malepativikas@gmail.com>
2019-01-24 20:12:40 -0500

    Created simple PID controller class. -VM
    Created simple PID controller class. -VM

--- DeepSpaceRobot/src/main/java/frc/utils/PIDController.java
+++ DeepSpaceRobot/src/main/java/frc/utils/PIDController.java
@@ -2,6 +2,8 @@ package frc.utils;
 
 public class PIDController {
 
+    private double m_P;                                 // factor for "proportional" control
+    private double m_I;                                 // factor for "integral" control
+    private double m_D;                                 // factor for "derivative" control
 
-    
 }
