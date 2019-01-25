37a9b8faf24fef845722519416c845397bcf1dc6
Luke Chen <lukekchenkk@gmail.com>
2019-01-25 13:07:23 -0500

    Set up speed tests, started driver take over test

--- DeepSpaceRobot/src/test/java/frc/robot/TransformTest.java
+++ DeepSpaceRobot/src/test/java/frc/robot/TransformTest.java
@@ -1,16 +1,53 @@
 package frc.robot;
 
 import frc.robot.command.teleop.Sqrt;
+import frc.robot.command.teleop.BarryAllen;
+import frc.robot.command.teleop.NormalSpeed;
 import frc.robot.command.teleop.Transform;
-import junit.framework.Assert;
+import frc.robot.command.teleop.HalfSpeed;
+import frc.robot.command.teleop.Sigmoid;
 
+import org.junit.Assert;
 import org.junit.Test;
 
-public class ControllerSqrtTest {
+public class TransformTest {
+
     @Test
     public void SqrtTest() {
         Transform sqrt = new Sqrt();
 
-        Assert.assertEquals(3, sqrt.transform(input));
+        Assert.assertEquals(3, sqrt.transform(9));
     }
+
+    @Test
+    public void NormalTest() {
+        Transform norm = new NormalSpeed();
+
+        Assert.assertEquals(1, norm.transform(1));
+    }
+
+    @Test
+    public void SigmoidTest() {
+        Transform sig = new Sigmoid();
+        double input = 1;
+        Assert.assertEquals(1 / (1 + Math.pow(Math.E, -input)), sig.transform(1));
+    }
+
+    @Test
+    public void SuperSaiyanTest() {
+        Transform goku = new BarryAllen();
+        Assert.assertEquals(1, goku.transform(1));
+    }
+
+    @Test
+    public void HalfSpeedTest() {
+        Transform haf = new HalfSpeed();
+        Assert.assertEquals(.0, haf.transform(1));
+    }
+
+    @Test
+    public void TakeOverTest() {
+        
+    }
+
 }
