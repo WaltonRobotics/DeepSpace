12c0cc48e2c848b1d9519ddd686701a038ddc7c4
vmalepati1 <malepativikas@gmail.com>
2019-01-26 10:56:58 -0500

    Finished info logging test. -VM
    Finished info logging test. -VM

--- DeepSpaceRobot/src/test/java/frc/robot/LoggerTest.java
+++ DeepSpaceRobot/src/test/java/frc/robot/LoggerTest.java
@@ -1,12 +1,28 @@
 package frc.robot;
 
+import java.io.ByteArrayOutputStream;
+import java.io.OutputStream;
+import frc.utils.Logger;
+
 import org.junit.Test;
 
 public class LoggerTest {
 
     @Test
-    public void testInfoLogging() {
-        
+    public void loggingInfoTest() {
+        ByteArrayOutputStream os;
+
+        List writeables = new ArrayList<OutputStream>();
+
+        writeables.add(os);
+
+        Logger logger = new Logger(writeables);
+
+        String testLogString = "Logging info test.";
+
+        logger.logInfo(testLogString);
+
+        assertEquals(os.toString(), testLogString);
     }
 
 }
