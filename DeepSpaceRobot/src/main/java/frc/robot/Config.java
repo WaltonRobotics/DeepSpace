3c8753d2f0a7a0727a90f3fa6e4e61c2301fd8bd
NamelessSuperCoder <Marius.juston@hotmail.fr>
2019-01-26 22:23:02 -0500

    saved the constants to Config
    still need to be improved but work relatively well when going straight

--- DeepSpaceRobot/src/main/java/frc/robot/Config.java
+++ DeepSpaceRobot/src/main/java/frc/robot/Config.java
@@ -24,5 +24,31 @@ public class Config {
     public static final int RIGHT_ENCODER_CHANNEL2 = 1; // digital
     public static final double DISTANCE_PER_PULSE = 0.00055805; // digital
   }
+
+  public static final class Constants {
+
+    //    Give the relationship between power and speed, calculated using the motion profiler log display
+    public static final double kV = 0.5349180715909608;
+    public static final double kK = -0.02306185102694297;
+    public static final double kAcc = 0.28150490814209445;
+
+    //    Constants to help tune the movement of the robot
+    public static final double kS = 0.8;
+    public static final double kAng = 0;
+    public static final double kL = 2;
+    public static final double iL = 0;
+    public static final double iAng = 0;
+
+    //    max velocity and acceleration the robot is should be able to go at
+    public static final double maxVelocity = (1 - kK) / kV;
+    public static final double maxAcceleration = 4;
+
+    /*
+    width of the robot. found by measuring the distance between the middle of the two wheel or,
+    more easily by measuring from the outside of a wheel to the inside of the other
+     */
+    public static final double robotWidth = 0.7500;
+
+
   }
 }
