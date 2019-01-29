0ae4602085061be9c76518679fdf4d614ab7c510
Pratheekl <35402973+Pratheekl@users.noreply.github.com>
2019-01-28 20:04:10 -0500

    Added some tests

---
+++ DeepSpaceRobot/src/test/java/frc/robot/EncoderTest.java
@@ -0,0 +1,46 @@
+package frc.robot;
+
+import edu.wpi.first.wpilibj.Encoder;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeAll;
+
+import org.junit.jupiter.api.Test;
+
+import static frc.team2974.robot.RobotMap.encoderLeft;
+import static frc.team2974.robot.RobotMap.encoderRight;
+
+public class EncoderTest {
+
+    private static Encoder encoder;
+    private static double distancePerPulse;
+
+    @BeforeAll
+    public static void init() {
+        distancePerPulse = 0.0002045;   // arbitrary DPP to be changed when we get a robot
+
+        encoderLeft.setDistancePerPulse(distancePerPulse);
+        encoderRight.setDistancePerPulse(distancePerPulse);
+
+        encoder = new Encoder(-1, 1);
+        encoder.setDistancePerPulse(distancePerPulse);
+    }
+
+    @Test
+    public void encoderTest() {
+
+        Assertions.assertEquals(distancePerPulse, encoder.getDistancePerPulse(), .005);
+    }
+
+    @Test
+    public void encoderLeftTest()
+    {
+        Assertions.assertEquals(distancePerPulse, encoderLeft.getDistancePerPulse(), .005);
+    }
+
+    @Test
+    public void encoderRightTest()
+    {
+        Assertions.assertEquals(distancePerPulse, encoderRight.getDistancePerPulse(), .005);
+    }
+}
