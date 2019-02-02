7854a48f125d1e295b8b46e60eb49137a8bf44d8
Luke Chen <lukekchenkk@gmail.com>
2019-02-02 11:39:32 -0500

    fixed minor issue

--- DeepSpaceRobot/src/main/java/frc/robot/Command/teleop/Sqrt.java
+++ DeepSpaceRobot/src/main/java/frc/robot/Command/teleop/Sqrt.java
@@ -4,7 +4,7 @@ public class Sqrt implements Transform{
 
     @Override
     public double transform(double input) {
-        return Math.signum(input) * Math.sqrt(Math.abs(input));;
+        return Math.signum(input) * Math.sqrt(Math.abs(input));
     }
 
 }
