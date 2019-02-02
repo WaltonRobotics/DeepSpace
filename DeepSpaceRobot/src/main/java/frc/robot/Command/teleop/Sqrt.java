45e2cee4ce2778bebd8cc8e6c47ca4bca83f8f93
vmalepati1 <malepativikas@gmail.com>
2019-02-02 11:30:00 -0500

    Removed printing in sqrt. -VM
    Removed printing in sqrt. -VM

--- DeepSpaceRobot/src/main/java/frc/robot/Command/teleop/Sqrt.java
+++ DeepSpaceRobot/src/main/java/frc/robot/Command/teleop/Sqrt.java
@@ -4,10 +4,7 @@ public class Sqrt implements Transform{
 
     @Override
     public double transform(double input) {
-        System.out.println(input);
-        double result = Math.signum(input) * Math.sqrt(Math.abs(input));
-        System.out.println(result);
-        return result;
+        return Math.signum(input) * Math.sqrt(Math.abs(input));;
     }
 
 }
