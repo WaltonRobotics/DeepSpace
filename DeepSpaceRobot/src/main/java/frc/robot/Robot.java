f109fc94cd3b3336d37e27bd3382cfb28a74ef4d
NamelessSuperCoder <Marius.juston@hotmail.fr>
2019-02-01 21:49:28 -0500

    saved pathdata release
    once the robot is disabled, the motion logger will save its current information and the remove that infor.
    This means that when you save the file it will only save the paths that were run  during that single enable/disable cycle allowing use to not have to redeploy code to see only the most current motion saved

--- DeepSpaceRobot/src/main/java/frc/robot/Robot.java
+++ DeepSpaceRobot/src/main/java/frc/robot/Robot.java
@@ -75,7 +75,7 @@ public class Robot extends TimedRobot {
   @Override
   public void disabledInit() {
     drivetrain.cancelControllerMotion();
-    drivetrain.getMotionLogger().writeMotionDataCSV();
+    drivetrain.getMotionLogger().writeMotionDataCSV(true);
   }
 
   @Override
