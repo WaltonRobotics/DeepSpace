0b1572a78fda2c96fb910d6c7246d67ec2533479
NamelessSuperCoder <Marius.juston@hotmail.fr>
2019-02-02 23:38:30 -0500

    set constants fro powerup

--- DeepSpaceRobot/src/main/java/frc/robot/Config.java
+++ DeepSpaceRobot/src/main/java/frc/robot/Config.java
@@ -13,42 +13,41 @@ public class Config {
 
   public static final class Hardware {
 
-    public static final int LEFT_WHEEL_CHANNEL = 0;
-    public static final int RIGHT_WHEEL_CHANNEL = 1;
     public static final int SHIFTER_CHANNEL = 0;
 
-    public static final int LEFT_ENCODER_CHANNEL1 = 2;
-    public static final int LEFT_ENCODER_CHANNEL2 = 3; // for second digital input
+    public static final int LEFT_ENCODER_CHANNEL1 = 0;
+    public static final int LEFT_ENCODER_CHANNEL2 = 1; // for second digital input
 
-    public static final int RIGHT_ENCODER_CHANNEL1 = 0; // digital
-    public static final int RIGHT_ENCODER_CHANNEL2 = 1; // digital
-    public static final double DISTANCE_PER_PULSE = 0.00055805; // digital
+    public static final int RIGHT_ENCODER_CHANNEL1 = 2; // digital
+    public static final int RIGHT_ENCODER_CHANNEL2 = 3; // digital
+    public static final double DISTANCE_PER_PULSE = 0.0002045; // digital
   }
 
   public static final class Constants {
 
     //    Give the relationship between power and speed, calculated using the motion profiler log display
-    public static final double kV = 0.5349180715909608;
-    public static final double kK = -0.02306185102694297;
-    public static final double kAcc = 0.28150490814209445;
+    public static final double kV = 0.194350;
+    public static final double kK = 0.194350;
+    public static final double kAcc = 0.125;
 
     //    Constants to help tune the movement of the robot
-    public static final double kS = 0.85;
-    public static final double kAng = 0.3;
+    public static final double kS = -2;
+    public static final double kAng = -1;
 
-    public static final double kL = 2;
+    public static final double kL =- 2;
     public static final double iL = 0;
     public static final double iAng = 0;
 
     //    max velocity and acceleration the robot is should be able to go at
-    public static final double maxVelocity = (1 - kK) / kV;
-    public static final double maxAcceleration = 4;
+//    public static final double maxVelocity = (1 - kK) / kV;
+    public static final double maxVelocity = 2.5;
+    public static final double maxAcceleration = 3.5;
 
     /*
     width of the robot. found by measuring the distance between the middle of the two wheel or,
     more easily by measuring from the outside of a wheel to the inside of the other
      */
-    public static final double robotWidth = 0.7500;
+    public static final double robotWidth = 0.7800;
   }
 
   public static final class SmartDashboardKeys {
@@ -61,8 +60,5 @@ public class Config {
     public static final String CONSTANTS_KACC = "Constants/KAcc";
     public static final String CONSTANTS_KK = "Constants/KK";
     public static final String CONSTANTS_KS = "Constants/KS";
-
-    public static final String MAX_VELOCITY = "Constants/Max Velocity";
-    public static final String MAX_ACCELERATION = "Constants/Max Acceleration";
   }
 }
