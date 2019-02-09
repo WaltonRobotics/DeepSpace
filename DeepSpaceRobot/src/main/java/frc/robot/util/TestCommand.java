718a9f1462f32a9d46c15b0eb22b378b4b5d620f
NamelessSuperCoder <Marius.juston@hotmail.fr>
2019-02-09 16:30:25 -0500

    added test results for each step of the command iteration
    returns test results as a list

--- DeepSpaceRobot/src/main/java/frc/robot/util/TestCommand.java
+++ DeepSpaceRobot/src/main/java/frc/robot/util/TestCommand.java
@@ -1,34 +1,52 @@
 package frc.robot.util;
 
 import edu.wpi.first.wpilibj.command.Command;
-import frc.robot.util.TestRunner.TestResult;
+import java.util.ArrayList;
+import java.util.List;
 
 public abstract class TestCommand extends Command {
 
-  private TestResult testResult = null;
+  private List<TestResult> testResult = new ArrayList<>();
+
+  public abstract String getTestName();
 
   @Override
   protected void initialize() {
-    initializeTest();
+    try {
+      initializeTest();
+      testResult.add(new TestResult(String.format("%s %s", getTestName(), "initialize"), true, null));
+    } catch (AssertionError error) {
+      testResult.add(new TestResult(String.format("%s %s", getTestName(), "initialize"), false, error));
+    }
   }
 
   @Override
   protected void execute() {
-    executeTest();
+    try {
+      executeTest();
+      testResult.add(new TestResult(String.format("%s %s", getTestName(), "execute"), true, null));
+    } catch (AssertionError error) {
+      testResult.add(new TestResult(String.format("%s %s", getTestName(), "execute"), false, error));
+    }
   }
 
   @Override
   protected void end() {
-    testResult = endTest();
+    try {
+      endTest();
+      testResult.add(new TestResult(String.format("%s %s", getTestName(), "end"), true, null));
+    } catch (AssertionError error) {
+      testResult.add(new TestResult(String.format("%s %s", getTestName(), "end"), false, error));
+    }
   }
 
   protected abstract void initializeTest();
 
   protected abstract void executeTest();
 
-  protected abstract TestResult endTest();
+  protected abstract void endTest();
 
-  public TestResult getTestResult() {
+  public List<TestResult> getTestResult() {
     return testResult;
   }
 }
