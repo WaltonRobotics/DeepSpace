04634978719892eb87801a5764f0bbc1c3843939
RishabRao <rishabpaws@gmail.com>
2019-02-12 19:24:04 -0500

    Revert "Update HatchIntaker.java"
    This reverts commit fd98610d69bdd17343437e876f8d6bef3069fdc7.

--- DeepSpaceRobot/src/main/java/frc/robot/subsystem/HatchIntaker.java
+++ DeepSpaceRobot/src/main/java/frc/robot/subsystem/HatchIntaker.java
@@ -34,7 +34,7 @@ public class HatchIntaker extends Subsystem {
   private HatchIntaker() {
   }
 
-  public static HatchIntaker getinstance() {
+  public static HatchIntaker getHinstance() {
     return instance;
   }
 
