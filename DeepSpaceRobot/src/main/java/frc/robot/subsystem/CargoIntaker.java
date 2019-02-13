83af4d1a472ae3f6d585a2df2fc782e0dfa60c58
RishabRao <rishabpaws@gmail.com>
2019-02-12 19:23:44 -0500

    Added Functions to CargoIntaker

--- DeepSpaceRobot/src/main/java/frc/robot/subsystem/CargoIntaker.java
+++ DeepSpaceRobot/src/main/java/frc/robot/subsystem/CargoIntaker.java
@@ -7,55 +7,43 @@
 
 package frc.robot.subsystem;
 
-import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.command.Subsystem;
 import frc.robot.command.teleop.CargoIntake;
 
+import static frc.robot.Gamepad.Button.RIGHT_BUMPER;
+import static frc.robot.Gamepad.Button.LEFT_BUMPER;
 import static frc.robot.RobotMap.leftCargoIntake;
 import static frc.robot.RobotMap.rightCargoIntake;
+import static frc.robot.OI.gamepad;
 
-/**
- * Add your docs here.
- */
+
 public class CargoIntaker extends Subsystem {
-  // Put methods for controlling this subsystem
-  // here. Call these from Commands.
-  //
 
   private static final CargoIntaker instance = new CargoIntaker();
 
-  private final Timer timer = new Timer();
-
 
   private CargoIntaker() {
-    timer.reset();
   }
 
   public static CargoIntaker getInstance() {
     return instance;
   }
 
+  public void activateIntake() {
+    if (gamepad.getButton(RIGHT_BUMPER)) {
+
+      rightCargoIntake.set(1);
+      leftCargoIntake.set(1);
+
+    }
+    else if (gamepad.getButton(LEFT_BUMPER)) {
+      rightCargoIntake.set(-1);
+      leftCargoIntake.set(-1);
+    }
+  }
+
   @Override
   public void initDefaultCommand() {
-    // Set the default command for a subsystem here.
     setDefaultCommand(new CargoIntake());
   }
-
-  /**
-   * Sets the motor powers.
-   */
-  private synchronized void setMotorPowers(double left, double right) {
-    leftCargoIntake.set(left);
-    rightCargoIntake.set(right);
-  }
-
-  public boolean timeElapsed(double time) {
-    return timer.hasPeriodPassed(time);
-  }
-
-  public synchronized void resetTimer() {
-    timer.reset();
-     timer.start();
-  }
-
 }
