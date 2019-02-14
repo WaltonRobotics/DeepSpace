81fb51fbaf4a76da3ce03e0644ee92eae66b07ed
RishabRao <rishabpaws@gmail.com>
2019-02-13 20:44:08 -0500

    Added setup and ending

--- DeepSpaceRobot/src/main/java/frc/robot/robotState/Defense.java
+++ DeepSpaceRobot/src/main/java/frc/robot/robotState/Defense.java
@@ -1,10 +1,14 @@
 package frc.robot.robotState;
 
+import frc.robot.Robot;
 import frc.robot.state.State;
+import frc.robot.subsystem.ElevatorCargoHatchSubsystem;
 
 public class Defense implements State {
     @Override
     public void initialize() {
+        Robot.godSubsystem.setCurrentActiveState(ElevatorCargoHatchSubsystem.ActiveState.DEFENSE);
+
 
     }
 
