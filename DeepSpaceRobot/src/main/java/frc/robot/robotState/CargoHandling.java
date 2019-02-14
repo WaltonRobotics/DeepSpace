62e52277eba7ed3dc60c2460973fb528721d6d97
RishabRao <rishabpaws@gmail.com>
2019-02-13 20:43:51 -0500

    Added setup and ending

--- DeepSpaceRobot/src/main/java/frc/robot/robotState/CargoHandling.java
+++ DeepSpaceRobot/src/main/java/frc/robot/robotState/CargoHandling.java
@@ -1,20 +1,28 @@
 package frc.robot.robotState;
 
+import frc.robot.Robot;
 import frc.robot.state.State;
+import frc.robot.subsystem.CargoIntaker;
+import frc.robot.subsystem.ElevatorCargoHatchSubsystem;
 
 public class CargoHandling implements State {
+
     @Override
     public void initialize() {
+        Robot.godSubsystem.setCurrentActiveState(ElevatorCargoHatchSubsystem.ActiveState.CARGO_HANDLING);
 
     }
 
     @Override
     public State periodic() {
+
         return null;
     }
 
     @Override
     public void finish() {
+
+        CargoIntaker.getInstance().outTakeCargo();
 
     }
 }
