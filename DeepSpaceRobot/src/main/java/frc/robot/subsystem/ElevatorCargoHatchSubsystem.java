662fc409691b980f4aada8ae21bc61717aabfe31
NamelessSuperCoder <Marius.juston@hotmail.fr>
2019-02-13 19:02:38 -0500

    created overall subsystem

---
+++ DeepSpaceRobot/src/main/java/frc/robot/subsystem/ElevatorCargoHatchSubsystem.java
@@ -0,0 +1,12 @@
+package frc.robot.subsystem;
+
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+public class ElevatorCargoHatchSubsystem extends Subsystem {
+
+  @Override
+  protected void initDefaultCommand() {
+
+  }
+}
