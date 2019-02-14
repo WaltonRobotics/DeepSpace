786ffb31f8fcfe33220e8854a42f0090c48bbe37
RishabRao <rishabpaws@gmail.com>
2019-02-13 20:44:21 -0500

    Added setup and ending

--- DeepSpaceRobot/src/main/java/frc/robot/robotState/HatchHandling.java
+++ DeepSpaceRobot/src/main/java/frc/robot/robotState/HatchHandling.java
@@ -1,11 +1,15 @@
 package frc.robot.robotState;
 
+import frc.robot.Robot;
 import frc.robot.state.State;
+import frc.robot.subsystem.ElevatorCargoHatchSubsystem;
+import frc.robot.subsystem.HatchIntaker;
 
 public class HatchHandling implements State {
     @Override
     public void initialize() {
-        
+        Robot.godSubsystem.setCurrentActiveState(ElevatorCargoHatchSubsystem.ActiveState.HATCH_HANDLING);
+
     }
 
     @Override
@@ -15,6 +19,8 @@ public class HatchHandling implements State {
 
     @Override
     public void finish() {
+
+        HatchIntaker.getHinstance().closeHatchIntake();
 
     }
 }
