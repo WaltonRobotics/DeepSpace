f0842ff1feb816c3655e8c1e452dad4db39ebd1a
AndrewLi8779 <42279294+AndrewLi8779@users.noreply.github.com>
2019-02-13 19:14:48 -0500

    Created the robotState classes

---
+++ DeepSpaceRobot/src/main/java/frc/robot/robotState/DefenseTransition.java
@@ -0,0 +1,20 @@
+package frc.robot.robotState;
+
+import frc.robot.state.State;
+
+public class DefenseTransition implements State {
+    @Override
+    public void initialize() {
+
+    }
+
+    @Override
+    public State periodic() {
+        return null;
+    }
+
+    @Override
+    public void finish() {
+
+    }
+}
