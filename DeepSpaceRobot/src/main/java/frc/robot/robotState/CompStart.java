87481bcbf8600f886aabfff433fd47d2009bb590
RishabRao <rishabpaws@gmail.com>
2019-02-13 20:45:20 -0500

    Added setup and ending

--- DeepSpaceRobot/src/main/java/frc/robot/robotState/CompStart.java
+++ DeepSpaceRobot/src/main/java/frc/robot/robotState/CompStart.java
@@ -1,10 +1,13 @@
 package frc.robot.robotState;
 
+import frc.robot.Robot;
 import frc.robot.state.State;
+import frc.robot.subsystem.ElevatorCargoHatchSubsystem;
 
 public class CompStart implements State {
     @Override
     public void initialize() {
+        Robot.godSubsystem.setCurrentActiveState(ElevatorCargoHatchSubsystem.ActiveState.ROBOT_SWITCHED_ON);
 
     }
 
