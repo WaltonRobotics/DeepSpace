4a091a4e16687eecab302ce249ad6a504a786689
RishabRao <rishabpaws@gmail.com>
2019-02-13 20:44:14 -0500

    Added setup and ending

--- DeepSpaceRobot/src/main/java/frc/robot/robotState/DefenseTransition.java
+++ DeepSpaceRobot/src/main/java/frc/robot/robotState/DefenseTransition.java
@@ -1,11 +1,19 @@
 package frc.robot.robotState;
 
+import frc.robot.Robot;
 import frc.robot.state.State;
+import frc.robot.subsystem.CargoIntaker;
+import frc.robot.subsystem.Elevator;
+import frc.robot.subsystem.ElevatorCargoHatchSubsystem;
+import frc.robot.subsystem.HatchIntaker;
 
 public class DefenseTransition implements State {
     @Override
     public void initialize() {
-
+        Robot.godSubsystem.setCurrentActiveState(ElevatorCargoHatchSubsystem.ActiveState.DEFENSE);
+        Elevator.getInstance().resetElevator();
+        HatchIntaker.getHinstance().flipInHatchIntake();
+        CargoIntaker.getInstance().flipInClawSystem();
     }
 
     @Override
