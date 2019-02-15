fb005b6f3ffd4a53602b5487dcd111a7f42587c8
Pratheekl <35402973+Pratheekl@users.noreply.github.com>
2019-02-14 21:01:17 -0500

    ADded hatch intake enum. implemented it in the ElevatorCargoHatchSubsystem

--- DeepSpaceRobot/src/main/java/frc/robot/robotState/CompStart.java
+++ DeepSpaceRobot/src/main/java/frc/robot/robotState/CompStart.java
@@ -5,15 +5,24 @@ import frc.robot.state.State;
 import frc.robot.subsystem.ElevatorCargoHatchSubsystem;
 
 public class CompStart implements State {
+
     @Override
     public void initialize() {
         Robot.godSubsystem.setCurrentActiveState(ElevatorCargoHatchSubsystem.ActiveState.ROBOT_SWITCHED_ON);
+
 
     }
 
     @Override
     public State periodic() {
-        return null;
+        ElevatorCargoHatchSubsystem.ActiveState currentActiveState = Robot.godSubsystem.getCurrentActiveState();
+
+        if (currentActiveState == ElevatorCargoHatchSubsystem.ActiveState.CARGO_HANDLING)
+            return new CargoHandlingTransition();
+        else if ((currentActiveState == ElevatorCargoHatchSubsystem.ActiveState.DEFENSE))
+            return new DefenseTransition();
+        else
+            return new HatchHandlingTransition();
     }
 
     @Override
