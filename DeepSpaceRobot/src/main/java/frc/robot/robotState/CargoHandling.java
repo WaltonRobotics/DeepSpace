66e1bfa6bf050736dea12892bf9ae1bf11a04cc1
RishabRao <rishabpaws@gmail.com>
2019-02-14 19:51:22 -0500

    updated cargo handling

--- DeepSpaceRobot/src/main/java/frc/robot/robotState/CargoHandling.java
+++ DeepSpaceRobot/src/main/java/frc/robot/robotState/CargoHandling.java
@@ -1,10 +1,18 @@
 package frc.robot.robotState;
 
 import frc.robot.Robot;
+import frc.robot.RobotMap;
 import frc.robot.state.State;
 import frc.robot.subsystem.ElevatorCargoHatchSubsystem;
 
+import static frc.robot.OI.*;
+
+
 public class CargoHandling implements State {
+
+    public boolean isFlipped() {
+        return RobotMap.clawRotationMotor.getActiveTrajectoryPosition() > 90;
+    }
 
     @Override
     public void initialize() {
@@ -15,13 +23,28 @@ public class CargoHandling implements State {
     @Override
     public State periodic() {
 
-        return null;
+    if(intakeCargoButton.get()) {
+        Robot.godSubsystem.intakeCargo();
     }
+
+    else if(outtakeCargoButton.get()) {
+        Robot.godSubsystem.outTakeCargo();
+    }
+
+    else if(flipCargoIntakeButton.get()) {
+
+        if(isFlipped())
+            Robot.godSubsystem.flipInClawSystem();
+
+        else
+            Robot.godSubsystem.flipOutClawSystem();
+    }
+
+    return null;
+}
 
     @Override
     public void finish() {
-
         Robot.godSubsystem.outTakeCargo();
-
     }
 }
