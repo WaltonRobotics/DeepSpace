5605481ec408adfaf5896b91118b4a42c2a86569
NamelessSuperCoder <Marius.juston@hotmail.fr>
2019-02-15 00:08:33 -0500

    reorganized godSubsystem into smaller inner classes

--- DeepSpaceRobot/src/main/java/frc/robot/subsystem/ElevatorCargoHatchSubsystem.java
+++ DeepSpaceRobot/src/main/java/frc/robot/subsystem/ElevatorCargoHatchSubsystem.java
@@ -15,40 +15,21 @@ import static frc.robot.RobotMap.*;
 
 public class ElevatorCargoHatchSubsystem extends Subsystem {
 
+  private Elevator elevator = new Elevator();
+  private Cargo cargo = new Cargo();
+  private Hatch hatch = new Hatch();
     private ActiveState currentActiveState = ActiveState.ROBOT_SWITCHED_ON;
-    private Timer elevatorRuntime;
-
-    private ElevatorControlMode elevatorControlMode;
-    private Logger elevatorLogger;
 
     private StateBuilder stateMachine;
 
     // Inputs
-    private boolean elevatorLastUpButtonPressed;
-    private boolean elevatorLastDownButtonPressed;
-    private boolean elevatorCurrentUpButtonPressed;
-    private boolean elevatorCurrentDownButtonPressed;
-    private int elevatorCurrentEncoderPosition;
 
-    private boolean cargoLastOutButtonPressed;
-    private boolean cargoLastInButtonPressed;
-    private boolean cargoLastFlipButtonPressed;
-    private boolean cargoCurrentOutButtonPressed;
-    private boolean cargoCurrentInButtonPressed;
-    private boolean cargoCurrentFlipButtonPressed;
 
     // Output
-    private double elevatorCurrentPower;
-    private double elevatorCurrentTarget;
-
-    private double hatchIntakeCurrentPower;
 
     // ???
 
     public ElevatorCargoHatchSubsystem() {
-        elevatorRuntime = new Timer();
-        elevatorLogger = new Logger();
-        elevatorControlMode = ElevatorControlMode.DISABLED;
         stateMachine = new StateBuilder(new Disabled());
 
         // Set sense of encoder
@@ -108,71 +89,6 @@ public class ElevatorCargoHatchSubsystem extends Subsystem {
         hatchRotationMotor.set(ControlMode.PercentOutput, -1);
     }
 
-    public void resetElevator() {
-        elevatorRuntime.reset();
-
-        elevatorLastUpButtonPressed = false;
-        elevatorLastDownButtonPressed = false;
-
-        elevatorCurrentPower = 0.0;
-        elevatorCurrentTarget = elevatorMotor.getSelectedSensorPosition(0);
-
-        elevatorControlMode = ElevatorControlMode.AUTO;
-    }
-
-    public boolean isElevatorUpButtonPressed() {
-        return elevatorCurrentUpButtonPressed;
-    }
-
-    public boolean isElevatorDownButtonPressed() {
-        return elevatorCurrentDownButtonPressed;
-    }
-
-    public boolean wasElevatorUpButtonPressed() {
-        return (elevatorCurrentUpButtonPressed != elevatorLastUpButtonPressed) && elevatorCurrentUpButtonPressed;
-    }
-
-    public boolean wasElevatorDownButtonPressed() {
-        return (elevatorCurrentDownButtonPressed != elevatorLastDownButtonPressed) && elevatorCurrentDownButtonPressed;
-    }
-
-    /* Get raw height of elevator from encoder ticks. */
-    public int getElevatorHeight() {
-        return elevatorCurrentEncoderPosition;
-    }
-
-    public ElevatorLevel getElevatorLevel() {
-        int currentHeight = getElevatorHeight();
-
-        if (currentHeight >= ElevatorLevel.BASE.target && currentHeight < ElevatorLevel.LEVEL1.target)
-            return ElevatorLevel.BASE;
-        if (currentHeight >= ElevatorLevel.LEVEL1.target && currentHeight < ElevatorLevel.LEVEL2.target)
-            return ElevatorLevel.LEVEL1;
-        if (currentHeight >= ElevatorLevel.LEVEL2.target && currentHeight < ElevatorLevel.LEVEL3.target)
-            return ElevatorLevel.LEVEL2;
-        if (currentHeight >= ElevatorLevel.LEVEL3.target) return ElevatorLevel.LEVEL3;
-
-        return ElevatorLevel.UNKNOWN;
-    }
-
-    public void setElevatorLevel(ElevatorLevel level) {
-        elevatorCurrentTarget = level.getTarget();
-    }
-
-    public double getElevatorPower() { return elevatorCurrentPower; }
-
-    public void setElevatorPower(double percent) {
-        elevatorCurrentPower = percent;
-    }
-
-    public ElevatorControlMode getElevatorControlMode() {
-        return elevatorControlMode;
-    }
-
-    public void setElevatorControlMode(ElevatorControlMode controlMode) {
-        this.elevatorControlMode = controlMode;
-    }
-
   /**
    *
    * @return if the getAngle() value is in the enumerated range above
@@ -194,10 +110,6 @@ public class ElevatorCargoHatchSubsystem extends Subsystem {
     }
   }
 
-  public double hatchIntakePower()
-  {
-    return hatchIntakeCurrentPower;
-  }
 
 
 
@@ -210,20 +122,9 @@ public class ElevatorCargoHatchSubsystem extends Subsystem {
 
     private void collectSensorData() {
         /* Read state of inputs. */
-        elevatorLastUpButtonPressed = elevatorCurrentUpButtonPressed;
-        elevatorCurrentUpButtonPressed = elevatorUpButton.get();
-
-        elevatorLastDownButtonPressed = elevatorCurrentDownButtonPressed;
-        elevatorCurrentDownButtonPressed = elevatorDownButton.get();
-
-        elevatorCurrentEncoderPosition = elevatorMotor.getSelectedSensorPosition(0);
-
-        cargoLastInButtonPressed = cargoCurrentInButtonPressed;
-        cargoCurrentInButtonPressed = intakeCargoButton.get();
-        cargoLastOutButtonPressed = cargoCurrentOutButtonPressed;
-        cargoCurrentOutButtonPressed = outtakeCargoButton.get();
-        cargoLastFlipButtonPressed = cargoCurrentFlipButtonPressed;
-        cargoCurrentFlipButtonPressed = flipCargoIntakeButton.get();
+      elevator.collectData();
+      cargo.collectData();
+      hatch.collectData();
     }
 
     private void processSensorData() {
@@ -232,26 +133,180 @@ public class ElevatorCargoHatchSubsystem extends Subsystem {
 
     private void output() {
         // Here's where we actually set the motors etc...
-        String logOutput = String.format("[%s]: Encoder height: %d, Current height target: %f, Current power: %f", elevatorRuntime.get(), getElevatorHeight(), elevatorCurrentTarget, getElevatorPower());
-        elevatorLogger.logInfo(logOutput);
 
-        switch (elevatorControlMode) {
-            case ZEROING:
-                elevatorMotor.set(ControlMode.PercentOutput, elevatorCurrentPower);
-                break;
-            case AUTO:
-                elevatorMotor.set(ControlMode.MotionMagic, elevatorCurrentTarget);
-                break;
-            case MANUAL:
-                elevatorMotor.set(ControlMode.PercentOutput, elevatorCurrentPower);
-                break;
-            default:
-                break;
-        }
+      elevator.outputData();
+      cargo.outputData();
+      hatch.outputData();
     }
 
     @Override
     protected void initDefaultCommand() {
+
+    }
+
+    public class Elevator implements SubSubsystem{
+      // Inputs
+      private boolean elevatorLastUpButtonPressed;
+      private boolean elevatorLastDownButtonPressed;
+      private boolean elevatorCurrentUpButtonPressed;
+      private boolean elevatorCurrentDownButtonPressed;
+      private int elevatorCurrentEncoderPosition;
+
+      // Output
+      private double elevatorCurrentPower;
+      private double elevatorCurrentTarget;
+
+      private Timer elevatorRuntime;
+
+      private ElevatorControlMode elevatorControlMode;
+      private Logger elevatorLogger;
+
+      public Elevator() {
+        elevatorRuntime = new Timer();
+        elevatorLogger = new Logger();
+        elevatorControlMode = ElevatorControlMode.DISABLED;
+      }
+
+      @Override
+      public void collectData() {
+        elevatorLastUpButtonPressed = elevatorCurrentUpButtonPressed;
+        elevatorCurrentUpButtonPressed = elevatorUpButton.get();
+
+        elevatorLastDownButtonPressed = elevatorCurrentDownButtonPressed;
+        elevatorCurrentDownButtonPressed = elevatorDownButton.get();
+
+        elevatorCurrentEncoderPosition = elevatorMotor.getSelectedSensorPosition(0);
+
+      }
+
+      @Override
+      public void outputData() {
+        String logOutput = String.format("[%s]: Encoder height: %d, Current height target: %f, Current power: %f", elevatorRuntime.get(), getElevatorHeight(), elevatorCurrentTarget, getElevatorPower());
+        elevatorLogger.logInfo(logOutput);
+
+        switch (elevatorControlMode) {
+          case ZEROING:
+            elevatorMotor.set(ControlMode.PercentOutput, elevatorCurrentPower);
+            break;
+          case AUTO:
+            elevatorMotor.set(ControlMode.MotionMagic, elevatorCurrentTarget);
+            break;
+          case MANUAL:
+            elevatorMotor.set(ControlMode.PercentOutput, elevatorCurrentPower);
+            break;
+          default:
+            break;
+        }
+      }
+
+      public void resetElevator() {
+        elevatorRuntime.reset();
+
+        elevatorLastUpButtonPressed = false;
+        elevatorLastDownButtonPressed = false;
+
+        elevatorCurrentPower = 0.0;
+        elevatorCurrentTarget = elevatorMotor.getSelectedSensorPosition(0);
+
+        elevatorControlMode = ElevatorControlMode.AUTO;
+      }
+
+      public boolean isElevatorUpButtonPressed() {
+        return elevatorCurrentUpButtonPressed;
+      }
+
+      public boolean isElevatorDownButtonPressed() {
+        return elevatorCurrentDownButtonPressed;
+      }
+
+      public boolean wasElevatorUpButtonPressed() {
+        return (elevatorCurrentUpButtonPressed != elevatorLastUpButtonPressed) && elevatorCurrentUpButtonPressed;
+      }
+
+      public boolean wasElevatorDownButtonPressed() {
+        return (elevatorCurrentDownButtonPressed != elevatorLastDownButtonPressed) && elevatorCurrentDownButtonPressed;
+      }
+
+      /* Get raw height of elevator from encoder ticks. */
+      public int getElevatorHeight() {
+        return elevatorCurrentEncoderPosition;
+      }
+
+      public ElevatorLevel getElevatorLevel() {
+        int currentHeight = getElevatorHeight();
+
+        if (currentHeight >= ElevatorLevel.BASE.target && currentHeight < ElevatorLevel.LEVEL1.target)
+          return ElevatorLevel.BASE;
+        if (currentHeight >= ElevatorLevel.LEVEL1.target && currentHeight < ElevatorLevel.LEVEL2.target)
+          return ElevatorLevel.LEVEL1;
+        if (currentHeight >= ElevatorLevel.LEVEL2.target && currentHeight < ElevatorLevel.LEVEL3.target)
+          return ElevatorLevel.LEVEL2;
+        if (currentHeight >= ElevatorLevel.LEVEL3.target) return ElevatorLevel.LEVEL3;
+
+        return ElevatorLevel.UNKNOWN;
+      }
+
+      public void setElevatorLevel(ElevatorLevel level) {
+        elevatorCurrentTarget = level.getTarget();
+      }
+
+      public double getElevatorPower() { return elevatorCurrentPower; }
+
+      public void setElevatorPower(double percent) {
+        elevatorCurrentPower = percent;
+      }
+
+      public ElevatorControlMode getElevatorControlMode() {
+        return elevatorControlMode;
+      }
+
+      public void setElevatorControlMode(ElevatorControlMode controlMode) {
+        this.elevatorControlMode = controlMode;
+      }
+    }
+    public class Cargo implements SubSubsystem{
+      // Inputs
+      private boolean cargoLastOutButtonPressed;
+      private boolean cargoLastInButtonPressed;
+      private boolean cargoLastFlipButtonPressed;
+      private boolean cargoCurrentOutButtonPressed;
+      private boolean cargoCurrentInButtonPressed;
+      private boolean cargoCurrentFlipButtonPressed;
+
+      @Override
+      public void collectData() {
+        cargoLastInButtonPressed = cargoCurrentInButtonPressed;
+        cargoCurrentInButtonPressed = intakeCargoButton.get();
+        cargoLastOutButtonPressed = cargoCurrentOutButtonPressed;
+        cargoCurrentOutButtonPressed = outtakeCargoButton.get();
+        cargoLastFlipButtonPressed = cargoCurrentFlipButtonPressed;
+        cargoCurrentFlipButtonPressed = flipCargoIntakeButton.get();
+      }
+
+      @Override
+      public void outputData() {
+
+      }
+    }
+    public class Hatch implements SubSubsystem{
+      // Output
+
+      private double hatchIntakeCurrentPower;
+
+      @Override
+      public void collectData() {
+
+      }
+
+      @Override
+      public void outputData() {
+
+      }
+
+      public double hatchIntakePower()
+      {
+        return hatchIntakeCurrentPower;
+      }
 
     }
 
@@ -306,5 +361,4 @@ public class ElevatorCargoHatchSubsystem extends Subsystem {
       return Math.abs(angle - this.angle) < 10;
     }
   }
-
 }
