2e8fe341ae01af28d7909abc1c902d999287bbae
RishabRao <rishabpaws@gmail.com>
2019-02-16 13:52:00 -0500

    Updated CargoHandling

--- DeepSpaceRobot/src/main/java/frc/robot/robotState/CargoHandling.java
+++ DeepSpaceRobot/src/main/java/frc/robot/robotState/CargoHandling.java
@@ -1,51 +1,28 @@
 package frc.robot.robotState;
 
 import frc.robot.Robot;
-import frc.robot.RobotMap;
 import frc.robot.state.State;
 import frc.robot.subsystem.ElevatorCargoHatchSubsystem;
 
-import static frc.robot.OI.*;
-
-public
-
 public class CargoHandling implements State {
 
-    public boolean isFlipped() {
-        return RobotMap.clawRotationMotor.getActiveTrajectoryPosition() > 90;
-    }
 
-    @Override
-    public void initialize() {
-        Robot.godSubsystem.setCurrentActiveState(ElevatorCargoHatchSubsystem.ActiveState.CARGO_HANDLING);
+  @Override
+  public void initialize() {
+    Robot.godSubsystem.setCurrentActiveState(ElevatorCargoHatchSubsystem.ActiveState.CARGO_HANDLING);
+  }
 
-    }
+  @Override
+  public State periodic() {
 
-    @Override
-    public State periodic() {
+    if(Robot.godSubsystem.defenceModeRising())
+      return new DefenseTransition();
 
-    if() {
-        Robot.godSubsystem.getCargo().intakeCargo();
-    }
+    return this;
+  }
 
-    else if(outtakeCargoButton.get()) {
-        Robot.godSubsystem.getCargo().outtakeCargo();
-    }
-
-    else if(flipCargoIntakeButton.get()) {
-
-        if (isFlipped()) {
-            Robot.godSubsystem.flipInClawSystem();
-        } else {
-            Robot.godSubsystem.flipOutClawSystem();
-        }
-    }
-
-    return null;
-}
-
-    @Override
-    public void finish() {
-        Robot.godSubsystem.outTakeCargo();
-    }
+  @Override
+  public void finish() {
+    Robot.godSubsystem.getCargo().outtakeCargo(1000);
+  }
 }
