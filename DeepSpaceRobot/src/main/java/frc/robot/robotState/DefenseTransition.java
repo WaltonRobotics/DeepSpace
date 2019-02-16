a8df7aa37cef6d28cc29344a5927c540fd32f490
AndrewLi8779 <42279294+AndrewLi8779@users.noreply.github.com>
2019-02-16 12:06:01 -0500

    Fixed defense transition, removed unecessary finish in hatchhandling, and fixed style config

--- DeepSpaceRobot/src/main/java/frc/robot/robotState/DefenseTransition.java
+++ DeepSpaceRobot/src/main/java/frc/robot/robotState/DefenseTransition.java
@@ -1,30 +1,45 @@
 package frc.robot.robotState;
 
 import frc.robot.Robot;
-import frc.robot.RobotMap;
 import frc.robot.state.State;
 import frc.robot.subsystem.ElevatorCargoHatchSubsystem;
+import frc.robot.subsystem.ElevatorCargoHatchSubsystem.ActiveState;
 
 public class DefenseTransition implements State {
+
+    private ActiveState lastState;
+
     @Override
     public void initialize() {
+        if(Robot.godSubsystem.getCurrentActiveState() == ElevatorCargoHatchSubsystem.ActiveState.CARGO_HANDLING){
+            lastState = ActiveState.CARGO_HANDLING;
+        }
+        if(Robot.godSubsystem.getCurrentActiveState() == ElevatorCargoHatchSubsystem.ActiveState.HATCH_HANDLING){
+            lastState = ActiveState.HATCH_HANDLING;
+        }
+        if(Robot.godSubsystem.getCurrentActiveState() == ActiveState.DEFENSE){
+            lastState = ActiveState.DEFENSE;
+        }
         Robot.godSubsystem.setCurrentActiveState(ElevatorCargoHatchSubsystem.ActiveState.DEFENSE);
         Robot.godSubsystem.getElevator().resetElevator();
+        if(lastState == ActiveState.HATCH_HANDLING) {
+            if (!ElevatorCargoHatchSubsystem.HatchPosition.SAFE.inRange(Robot.godSubsystem.getHatch().getAngle())) {
+                Robot.godSubsystem.flipInHatchIntake();
+            }
+        }
+        if(lastState == ActiveState.CARGO_HANDLING){
+            if (!ElevatorCargoHatchSubsystem.HatchPosition.SAFE.inRange(Robot.godSubsystem.getHatch().getAngle())) {
+                Robot.godSubsystem.flipOutHatchIntake();
+            }
+            if (ElevatorCargoHatchSubsystem.CargoPosition.SAFE.inRange(Robot.godSubsystem.getCargo().getAngle())) {
+                Robot.godSubsystem.getCargo().flipInClawSystem();
+            }
+        }
     }
 
     @Override
     public State periodic() {
-        //not exactly sure how to do this but the logic is there
-        while (RobotMap.hatchPotentiometer.get() < 100){
-            Robot.godSubsystem.flipOutHatchIntake();
-        }
-        while (RobotMap.hatchPotentiometer.get() > 100){
-            Robot.godSubsystem.flipInHatchIntake();
-        }
-        while (RobotMap.cargoPotentiometer.get() > 100){
-            Robot.godSubsystem.flipInClawSystem();
-        }
-        return null;
+        return this;
     }
 
     @Override
