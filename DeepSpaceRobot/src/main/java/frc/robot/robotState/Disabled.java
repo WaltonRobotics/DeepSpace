eb460c708970a26cae4a89c570439d4ca13c1fc1
Pratheekl <35402973+Pratheekl@users.noreply.github.com>
2019-02-16 14:34:09 -0500

    updated disabled

--- DeepSpaceRobot/src/main/java/frc/robot/robotState/Disabled.java
+++ DeepSpaceRobot/src/main/java/frc/robot/robotState/Disabled.java
@@ -2,23 +2,26 @@ package frc.robot.robotState;
 
 import frc.robot.Robot;
 import frc.robot.state.State;
+import frc.robot.subsystem.Drivetrain;
 import frc.robot.subsystem.ElevatorCargoHatchSubsystem;
+import frc.robot.subsystem.ElevatorCargoHatchSubsystem.ClawControlMode;
+import frc.robot.subsystem.ElevatorCargoHatchSubsystem.ElevatorControlMode;
+import frc.robot.subsystem.ElevatorCargoHatchSubsystem.HatchControlMode;
 
 public class Disabled implements State {
     @Override
     public void initialize() {
-
-
-
+        Robot.godSubsystem.getHatch().setHatchControlMode(HatchControlMode.DISABLED);
+        Robot.godSubsystem.getCargo().setClawControlMode(ClawControlMode.DISABLED);
+        Robot.godSubsystem.getElevator().setElevatorControlMode(ElevatorControlMode.DISABLED);
     }
 
     @Override
     public State periodic() {
-        return this;
+        
     }
 
     @Override
     public void finish() {
-
     }
 }
