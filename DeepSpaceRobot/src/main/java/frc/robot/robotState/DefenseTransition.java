77eb7cb7a80aa4db0a2b6750ab0c268fe2bb9ba8
RishabRao <rishabpaws@gmail.com>
2019-02-16 16:10:48 -0500

    fixed defense transition

--- DeepSpaceRobot/src/main/java/frc/robot/robotState/DefenseTransition.java
+++ DeepSpaceRobot/src/main/java/frc/robot/robotState/DefenseTransition.java
@@ -2,34 +2,19 @@ package frc.robot.robotState;
 
 import frc.robot.Robot;
 import frc.robot.state.State;
-import frc.robot.subsystem.ElevatorCargoHatchSubsystem;
 import frc.robot.subsystem.ElevatorCargoHatchSubsystem.ActiveState;
 import frc.robot.subsystem.ElevatorCargoHatchSubsystem.CargoPosition;
 import frc.robot.subsystem.ElevatorCargoHatchSubsystem.HatchPosition;
 
 public class DefenseTransition implements State {
 
-    private ActiveState lastState;
 
     @Override
     public void initialize() {
-        switch (Robot.godSubsystem.getCurrentActiveState()){
-            case HATCH_HANDLING:
-                lastState = ActiveState.HATCH_HANDLING;
-            case CARGO_HANDLING:
-                lastState = ActiveState.CARGO_HANDLING;
-            case DEFENSE:
-                lastState = ActiveState.DEFENSE;
-        }
-        Robot.godSubsystem.setCurrentActiveState(ElevatorCargoHatchSubsystem.ActiveState.DEFENSE);
-        Robot.godSubsystem.getElevator().resetElevator();
-        switch (lastState){
-            case CARGO_HANDLING:
-                Robot.godSubsystem.getHatch().setHatchTarget(HatchPosition.SAFE);
-                Robot.godSubsystem.getCargo().setClawTarget(CargoPosition.SAFE);
-            case HATCH_HANDLING:
-                Robot.godSubsystem.getHatch().setHatchTarget(HatchPosition.SAFE);
-        }
+        Robot.godSubsystem.setCurrentActiveState(ActiveState.DEFENSE);
+        Robot.godSubsystem.getHatch().setHatchTarget(HatchPosition.SAFE);
+        Robot.godSubsystem.getCargo().setClawTarget(CargoPosition.SAFE);
+
     }
 
     @Override
@@ -42,10 +27,10 @@ public class DefenseTransition implements State {
         int cargoAngle = Robot.godSubsystem.getCargo().getAngle();
         int hatchAngle = Robot.godSubsystem.getHatch().getAngle();
 
-        if(CargoPosition.SAFE.isClose(cargoAngle) && HatchPosition.SAFE.isClose(hatchAngle)){
+        if(CargoPosition.SAFE.isClose(cargoAngle) && HatchPosition.SAFE.isClose(hatchAngle))
             return new Defense();
-        }
-        return this;
+        else
+            return this;
     }
 
     @Override
