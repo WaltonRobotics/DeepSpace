20896609bfa9975dab81cbbac5727fb9fd8637ea
NamelessSuperCoder <Marius.juston@hotmail.fr>
2019-02-16 10:29:44 -0500

    added initialization of sub subsystem
    used for seetting the inital limits of the target motors

--- DeepSpaceRobot/src/main/java/frc/robot/subsystem/ElevatorCargoHatchSubsystem.java
+++ DeepSpaceRobot/src/main/java/frc/robot/subsystem/ElevatorCargoHatchSubsystem.java
@@ -48,6 +48,9 @@ public class ElevatorCargoHatchSubsystem extends Subsystem {
   public ElevatorCargoHatchSubsystem() {
     stateMachine = new StateBuilder(new Disabled());
 
+    elevator.intialize();
+    hatch.intialize();
+    cargo.intialize();
     // Set sense of encoder
     // Set sense of motors
     // Set soft targets on
@@ -219,6 +222,11 @@ public class ElevatorCargoHatchSubsystem extends Subsystem {
       }
     }
 
+    @Override
+    public void intialize() {
+
+    }
+
     public void resetElevator() {
       elevatorRuntime.reset();
 
@@ -316,6 +324,11 @@ public class ElevatorCargoHatchSubsystem extends Subsystem {
     public void outputData() {
 
     }
+
+    @Override
+    public void intialize() {
+
+    }
   }
 
   public class Hatch implements SubSubsystem {
@@ -330,6 +343,11 @@ public class ElevatorCargoHatchSubsystem extends Subsystem {
 
     @Override
     public void outputData() {
+
+    }
+
+    @Override
+    public void intialize() {
 
     }
 
--- DeepSpaceRobot/src/main/java/frc/robot/subsystem/SubSubsystem.java
+++ DeepSpaceRobot/src/main/java/frc/robot/subsystem/SubSubsystem.java
@@ -3,4 +3,5 @@ package frc.robot.subsystem;
 public interface SubSubsystem {
   void collectData();
   void outputData();
+  void intialize();
 }
