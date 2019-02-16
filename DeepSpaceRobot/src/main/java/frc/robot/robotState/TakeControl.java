3983f5c219db93694e235baed8af2d262f1adca7
RishabRao <rishabpaws@gmail.com>
2019-02-16 15:56:18 -0500

    updated take control

--- DeepSpaceRobot/src/main/java/frc/robot/robotState/TakeControl.java
+++ DeepSpaceRobot/src/main/java/frc/robot/robotState/TakeControl.java
@@ -1,26 +1,53 @@
 package frc.robot.robotState;
 
+import edu.wpi.first.wpilibj.command.Scheduler;
 import frc.robot.Robot;
+import frc.robot.command.teleop.ZeroElevator;
 import frc.robot.state.State;
+import frc.robot.subsystem.ElevatorCargoHatchSubsystem;
+import frc.robot.subsystem.ElevatorCargoHatchSubsystem.ClawControlMode;
+import frc.robot.subsystem.ElevatorCargoHatchSubsystem.Elevator;
+import frc.robot.subsystem.ElevatorCargoHatchSubsystem.HatchControlMode;
 
 public class TakeControl implements State {
 
-    @Override
-    public void initialize() {
+  @Override
+  public void initialize() {
 
+    ElevatorCargoHatchSubsystem.Cargo cargo = Robot.godSubsystem.getCargo();
+    ElevatorCargoHatchSubsystem.Hatch hatch = Robot.godSubsystem.getHatch();
+    ElevatorCargoHatchSubsystem.Elevator elevator = Robot.godSubsystem.getElevator();
+
+    int currentCargoAngle = cargo.getAngle();
+    //FIXME:int elevatorPosition = elevator.getElevatorHeight();
+    int currentHatchAngle = hatch.getAngle();
+
+    cargo.setClawAngle(currentCargoAngle);
+    hatch.setHatchAngle(currentHatchAngle);
+
+    cargo.setClawControlMode(ClawControlMode.AUTO);
+    hatch.setHatchControlMode(HatchControlMode.AUTO);
+
+    elevator.setZeroed(false);
+    Scheduler.getInstance().add(new ZeroElevator());
+
+  }
+
+  @Override
+  public State periodic() {
+    if (!Robot.godSubsystem.isEnabled()) {
+      return new Disabled();
     }
 
-    @Override
-    public State periodic() {
-        if(!Robot.godSubsystem.isEnabled()){
-            return new Disabled();
-        }
-
-        return this;
+    if(Robot.godSubsystem.getElevator().isZeroed()) {
+      //TODO:
     }
+        return
+    return this;
+  }
 
-    @Override
-    public void finish() {
+  @Override
+  public void finish() {
 
-    }
+  }
 }
