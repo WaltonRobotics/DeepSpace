35af57b3ac19ee53abe69efaa5a1b88c3c4bd241
RishabRao <rishabpaws@gmail.com>
2019-02-16 18:22:33 -0500

    Update PracticeDeepSpace.java

--- DeepSpaceRobot/src/main/java/frc/robot/robot/PracticeDeepSpace.java
+++ DeepSpaceRobot/src/main/java/frc/robot/robot/PracticeDeepSpace.java
@@ -1,6 +1,12 @@
 package frc.robot.robot;
 
 
+import frc.robot.subsystem.ElevatorCargoHatchSubsystem.Cargo;
+import frc.robot.subsystem.ElevatorCargoHatchSubsystem.CargoPosition;
+import frc.robot.subsystem.ElevatorCargoHatchSubsystem.Elevator;
+import frc.robot.subsystem.ElevatorCargoHatchSubsystem.ElevatorLevel;
+import frc.robot.subsystem.ElevatorCargoHatchSubsystem.Hatch;
+import frc.robot.subsystem.ElevatorCargoHatchSubsystem.HatchPosition;
 import frc.robot.subsystem.TalonSRXConfig;
 import frc.robot.subsystem.SusystemTargets;
 import org.waltonrobotics.util.Controls;
@@ -181,6 +187,16 @@ public class PracticeDeepSpace extends LimitedRobot {
 
   @Override
   public void initLimits() {
+    this.getLimits().put(HatchPosition.SAFE, new LimitPair(70, 90));
+    this.getLimits().put(HatchPosition.DEPLOY, new LimitPair( 0, 90));
+    this.getLimits().put(HatchPosition.CARGO_START, new LimitPair(90, 100));
+    this.getLimits().put(HatchPosition.HATCH_START, new LimitPair(100, 110));
+    this.getLimits().put(CargoPosition.SAFE, new LimitPair(70, 90));
+    this.getLimits().put(CargoPosition.DEPLOY, new LimitPair(0, 90));
+    this.getLimits().put(ElevatorLevel.BASE, new LimitPair(0, 100));
+    this.getLimits().put(ElevatorLevel.LEVEL1, new LimitPair(100, 200));
+    this.getLimits().put(ElevatorLevel.LEVEL2, new LimitPair(200, 300));
+    this.getLimits().put(ElevatorLevel.LEVEL3, new LimitPair(300, 400));
 
   }
 }
