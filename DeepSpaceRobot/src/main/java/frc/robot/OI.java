ff5a6e2f5a2775c0a8c20a03777abf6b687bc42e
RishabRao <rishabpaws@gmail.com>
2019-02-17 11:49:56 -0500

    updated oi to add levels

--- DeepSpaceRobot/src/main/java/frc/robot/OI.java
+++ DeepSpaceRobot/src/main/java/frc/robot/OI.java
@@ -51,9 +51,9 @@ public class OI {
 
   public static final JoystickButton shiftUp = new JoystickButton(leftJoystick, SHIFT_UP_PORT);
   public static final JoystickButton shiftDown = new JoystickButton(leftJoystick, SHIFT_DOWN_PORT);
-  public static final JoystickButton elevatorLevel3 = new JoystickButton(gamepad, _4.index()); // All elevator joystick button ports are makeshift for now.
-  public static final JoystickButton elevatorLevel2 = new JoystickButton(gamepad, _3.index());
-  public static final JoystickButton elevatorLevel1 = new JoystickButton(gamepad, _2.index());
+  public static final JoystickButton elevatorLevel3Button = new JoystickButton(gamepad, _4.index()); // All elevator joystick button ports are makeshift for now.
+  public static final JoystickButton elevatorLevel2Button = new JoystickButton(gamepad, _3.index());
+  public static final JoystickButton elevatorLevel1Button = new JoystickButton(gamepad, _2.index());
   public static final JoystickButton elevatorZeroButton = new JoystickButton(gamepad, _1.index());
   public static final JoystickButton hatchIntakeButton = new JoystickButton(gamepad, RIGHT_TRIGGER.index());
   public static final JoystickButton intakeCargoButton = new JoystickButton(gamepad, RIGHT_BUMPER.index());
