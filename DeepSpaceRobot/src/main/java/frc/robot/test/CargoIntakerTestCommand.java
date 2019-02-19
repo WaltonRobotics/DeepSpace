785744e2bc447292e04a5ff8973f33012869ee9e
AndrewLi8779 <42279294+AndrewLi8779@users.noreply.github.com>
2019-02-18 19:36:16 -0500

    added cargo intake tests

--- DeepSpaceRobot/src/main/java/frc/robot/test/CargoIntakerTestCommand.java
+++ DeepSpaceRobot/src/main/java/frc/robot/test/CargoIntakerTestCommand.java
@@ -1,17 +1,37 @@
 package frc.robot.test;
 
+import frc.robot.Robot;
+import frc.robot.RobotMap;
+import frc.robot.subsystem.ElevatorCargoHatchSubsystem.CargoPosition;
+import frc.robot.subsystem.ElevatorCargoHatchSubsystem.ClawControlMode;
 import frc.robot.util.TestCommand;
 
 public class CargoIntakerTestCommand extends TestCommand {
 
   @Override
   protected void initializeTest() {
-
+    Robot.godSubsystem.getCargo().setClawControlMode(ClawControlMode.MANUAL);
+    Robot.godSubsystem.getCargo().setClawTarget(CargoPosition.DEPLOY);
   }
 
   @Override
   protected void executeTest() {
-
+    assert CargoPosition.DEPLOY.isClose(Robot.godSubsystem.getCargo().getAngle());
+    if(Robot.godSubsystem.getCargo().inButtonPressed()){
+      if (RobotMap.leftIntakeMotor.get() != 1 || RobotMap.rightIntakeMotor.get() != 1) {
+        throw new AssertionError();
+      }
+    }
+    if(Robot.godSubsystem.getCargo().outSlowButtonPressed()){
+      if (RobotMap.leftIntakeMotor.get() != -.5 || RobotMap.rightIntakeMotor.get() != -.5) {
+        throw new AssertionError();
+      }
+    }
+    if(Robot.godSubsystem.getCargo().outFastButtonPressed()){
+      if (RobotMap.leftIntakeMotor.get() != -1 || RobotMap.rightIntakeMotor.get() != -1) {
+        throw new AssertionError();
+      }
+    }
   }
 
   @Override
@@ -21,6 +41,9 @@ public class CargoIntakerTestCommand extends TestCommand {
 
   @Override
   protected boolean isFinished() {
-    return true;
+    if(Robot.godSubsystem.getCargo().inButtonPressed() && Robot.godSubsystem.getCargo().outFastButtonPressed() && Robot.godSubsystem.getCargo().outSlowButtonPressed()){
+      return true;
+    }
+    return false;
   }
 }
