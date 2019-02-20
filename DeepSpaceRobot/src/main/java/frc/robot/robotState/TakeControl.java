b7ac11ae220fd0f72e6cdaa8fddc8ae51174da4a
NamelessSuperCoder <Marius.juston@hotmail.fr>
2019-02-20 12:12:41 -0500

    added rest after zeroing to make sure it has finished

--- DeepSpaceRobot/src/main/java/frc/robot/robotState/TakeControl.java
+++ DeepSpaceRobot/src/main/java/frc/robot/robotState/TakeControl.java
@@ -15,12 +15,15 @@ import frc.robot.subsystem.ElevatorCargoHatchSubsystem.HatchPosition;
 
 public class TakeControl implements State {
 
+  private double timeout;
+  private Elevator elevator = Robot.godSubsystem.getElevator();
+  private Cargo cargo = Robot.godSubsystem.getCargo();
+  private Hatch hatch = Robot.godSubsystem.getHatch();
+
   @Override
   public void initialize() {
-    Cargo cargo = Robot.godSubsystem.getCargo();
-    Hatch hatch = Robot.godSubsystem.getHatch();
-    Elevator elevator = Robot.godSubsystem.getElevator();
 
+    elevator.setElevatorControlMode(ElevatorControlMode.ZEROING);
     int currentCargoAngle = cargo.getAngle();
     //FIXME:int elevatorPosition = elevator.getElevatorHeight();
     int currentHatchAngle = hatch.getAngle();
@@ -31,6 +34,7 @@ public class TakeControl implements State {
     cargo.setClawControlMode(ClawControlMode.AUTO);
     hatch.setHatchControlMode(HatchControlMode.AUTO);
 
+//    elevator.setZeroed(true);
     elevator.setZeroed(false);
     cargo.setLimits(CargoPosition.SAFE);
     hatch.setLimits(HatchPosition.SAFE);
@@ -43,19 +47,29 @@ public class TakeControl implements State {
       return new Disabled();
     }
 
+    if (elevator.isLowerLimit()
+//        || (elevator.getElevatorHeight()  - elevator.getLastEncoderPosition()) >= 0
+    ) {
+      elevator.setZeroed(true);
+    } else {
+      timeout = Robot.godSubsystem.getCurrentTime() + 500;
+    }
+
     if (Robot.godSubsystem.getElevator().isZeroed()) {
-      //TODO:move to comp start mode
-      switch (Robot.godSubsystem.getCurrentActiveState()) {
-        case DEFENSE:
-          return new DefenseTransition();
-        case CARGO_HANDLING:
-          return new CargoHandlingTransition();
-        case HATCH_HANDLING:
-          return new HatchHandlingTransition();
-        case ROBOT_SWITCHED_ON:
-          return new HatchHandlingTransition();
+      if (Robot.godSubsystem.getCurrentTime() >= timeout) {
+        switch (Robot.godSubsystem.getCurrentActiveState()) {
+          case DEFENSE:
+            return new DefenseTransition();
+          case CARGO_HANDLING:
+            return new CargoHandlingTransition();
+          case HATCH_HANDLING:
+            return new HatchHandlingTransition();
+          case ROBOT_SWITCHED_ON:
+            return new HatchHandlingTransition();
+        }
       }
     }
+
     return this;
   }
 
