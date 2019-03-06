8cfbf5ff86e0ab2378f329172591b237990b84f7
NamelessSuperCoder <Marius.juston@hotmail.fr>
2019-03-05 23:54:58 -0500

    included camera filter algorithm to camera assist
    uses a sort of kalman filter with an offset that gets updated everytime camera data is found, applies a certain camera filter proportion to the old and new camera positions
    does not use the offset data if the robot is not closer than .5 meters
    if the robot is 1.5 meters away from the target applies a proportion to the central power

--- DeepSpaceRobot/src/main/java/frc/robot/command/teleop/Drive.java
+++ DeepSpaceRobot/src/main/java/frc/robot/command/teleop/Drive.java
@@ -8,9 +8,12 @@
 package frc.robot.command.teleop;
 
 import static frc.robot.Config.SmartDashboardKeys.CAMERA_DATA_ACTUAL;
+import static frc.robot.Config.SmartDashboardKeys.CAMERA_DATA_PROPORTIONAL_POWER;
 import static frc.robot.Config.SmartDashboardKeys.CAMERA_DATA_TARGET;
+import static frc.robot.Config.SmartDashboardKeys.CAMERA_DATA_TARGET_OFFSET;
 import static frc.robot.Config.SmartDashboardKeys.CAMERA_DATA_USES_AUTOASSIST;
 import static frc.robot.Config.SmartDashboardKeys.DEBUG_ACTUAL_TARGET;
+import static frc.robot.Config.SmartDashboardKeys.DEBUG_CAMERA_OFFSET;
 import static frc.robot.Config.SmartDashboardKeys.DEBUG_CHOSEN_TARGET;
 import static frc.robot.Config.SmartDashboardKeys.DEBUG_JUST_BEFORE;
 import static frc.robot.Config.SmartDashboardKeys.DRIVETRAIN_LEFT_JOYSTICK_Y;
@@ -37,7 +40,9 @@ import org.waltonrobotics.metadata.RobotPair;
 
 public class Drive extends Command {
 
-  public MotionLogger motionLogger = new MotionLogger();
+  private static final double cameraFilter = 0.5;
+  private MotionLogger motionLogger = new MotionLogger();
+  private Pose offset = new Pose(0, 0, 0);
   private boolean hasFound = false;
   private EnhancedBoolean rightTriggerPress = new EnhancedBoolean();
 
@@ -47,7 +52,7 @@ public class Drive extends Command {
     requires(Robot.drivetrain);
   }
 
-  public Transform getTransform() {
+  private Transform getTransform() {
     return Robot.transformSendableChooser.getSelected();
   }
 
@@ -79,9 +84,9 @@ public class Drive extends Command {
     leftYJoystick = transform.transform(leftYJoystick);
     rightYJoystick = transform.transform(rightYJoystick);
 
-      CameraData currentCameraData = drivetrain.getCurrentCameraData();
-//      CameraData currentCameraData = new CameraData(
     if (rightTriggerPress.get() && !hasFound) {
+      CameraData cameraData = drivetrain.getCameraData();
+//      CameraData cameraData = new CameraData(
 //          SmartDashboard.getNumber(CAMERA_DATA_X, 0),
 //          SmartDashboard.getNumber(CAMERA_DATA_Y, 0),
 //          SmartDashboard.getNumber(CAMERA_DATA_HEIGHT, 0),
@@ -90,11 +95,14 @@ public class Drive extends Command {
 //          SmartDashboard.getNumber(CAMERA_DATA_TIME, 0)
 //      );
 
-      SmartDashboard.putString(DEBUG_CHOSEN_TARGET, currentCameraData.toString());
-      SmartDashboard.putString(DEBUG_JUST_BEFORE, drivetrain.getActualPosition().toString());
-      if (currentCameraData.getNumberOfTargets() != 0) {
-        drivetrain.setStartingPosition(currentCameraData.getCameraPose());
+      if (cameraData.getNumberOfTargets() != 0) {
+        System.out.println("Found target");
+        SmartDashboard.putString(DEBUG_CHOSEN_TARGET, cameraData.toString());
+        SmartDashboard.putString(DEBUG_JUST_BEFORE, drivetrain.getActualPosition().toString());
+        drivetrain.setStartingPosition(cameraData.getCameraPose());
         SmartDashboard.putString(DEBUG_ACTUAL_TARGET, drivetrain.getActualPosition().toString());
+        offset = new Pose();
+
         hasFound = true;
       } else {
         hasFound = false;
@@ -109,19 +117,38 @@ public class Drive extends Command {
       if (cameraData.getTime() != -1) {
         Pose camera = cameraData.getCameraPose();
 
-        System.out.println(
-            "Difference: " + new Pose(actualPathData.getX() - camera.getX(), actualPathData.getY() - camera.getY(),
-                actualPathData.getAngle() - camera.getAngle()).toString());
+        Pose difference = new Pose(camera.getX() - actualPathData.getX(), camera.getY() - actualPathData.getY(),
+            camera.getAngle() - actualPathData.getAngle());
+
+        offset = new Pose(offset.getX() * (1 - cameraFilter) + difference.getX() * cameraFilter,
+            offset.getY() * (1 - cameraFilter) + difference.getY() * cameraFilter,
+            offset.getAngle() * (1 - cameraFilter) + difference.getAngle() * cameraFilter);
       }
 
       //Get cameradata and get 90 percent of it in
-      PathData targetPathData = new PathData(new Pose(actualPathData.getX(), 0));
-      ErrorVector currentError = MotionController.findCurrentError(targetPathData, actualPathData);
+      double distance = Math
+          .max(Math.abs(actualPathData.getX()) - .5, SmartDashboard.getNumber(CAMERA_DATA_PROPORTIONAL_POWER, .2));
+
+      PathData targetPathData = new PathData(new Pose(actualPathData.getX(), SmartDashboard.getNumber(
+          CAMERA_DATA_TARGET_OFFSET, 0)));
+
+      Pose correctedPosition = new Pose();
+      if (distance <= .5) {
+        correctedPosition = actualPathData
+            .offset(offset.getX(), offset.getY(), offset.getAngle()); //FIXME overload to use with Pose
+      }
+
+      ErrorVector currentError = MotionController.findCurrentError(targetPathData, correctedPosition);
 
       double centerPower = (leftYJoystick + rightYJoystick) / 2;
       double steerPowerXTE = Math.abs(centerPower) * currentRobot.getKS() * currentError.getXTrack();
 
       double steerPowerAngle = currentRobot.getKAng() * currentError.getAngle();
+
+      System.out.println(distance);
+      if (distance <= 1.5) {
+        centerPower = centerPower * distance;
+      }
 
       double steerPower = Math.max(-1.0, Math.min(1.0, steerPowerXTE + steerPowerAngle));
 
@@ -144,6 +171,9 @@ public class Drive extends Command {
               ),
               0,
               MotionState.MOVING));
+
+      SmartDashboard.putString(
+          DEBUG_CAMERA_OFFSET, offset.toString());
       SmartDashboard.putString(
           CAMERA_DATA_ACTUAL, actualPathData.toString());
       SmartDashboard.putString(
