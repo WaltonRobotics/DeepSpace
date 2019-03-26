05b0bfbad0ccaebc67a983f45f251740375d0b0a
RishabRao <rishabpaws@gmail.com>
2019-03-26 11:14:06 -0400

    Create SimpleAlignmentDrive.java

---
+++ DeepSpaceRobot/src/main/java/frc/robot/command/auton/SimpleAlignmentDrive.java
@@ -0,0 +1,30 @@
+package frc.robot.command.auton;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import frc.robot.RobotMap;
+import org.waltonrobotics.command.SimpleLine;
+
+
+public class SimpleAlignmentDrive {
+
+  public static double forwardAngle = 10;
+  public static double safetyDistance = 0.5;
+
+  public static void forwardPowerDrive(double distance) {
+    SimpleLine.lineWithDistance(distance - safetyDistance);
+  }
+
+  public static void fixAlignment(Double currentAngle) {
+
+    if (currentAngle != forwardAngle) {
+      if (currentAngle > forwardAngle) {
+        RobotMap.rightWheels.set(ControlMode.PercentOutput, .5);
+        RobotMap.leftWheels.set(ControlMode.PercentOutput, -.5);
+      }
+      else if (currentAngle < forwardAngle) {
+        RobotMap.rightWheels.set(ControlMode.PercentOutput, -.5);
+        RobotMap.leftWheels.set(ControlMode.PercentOutput, .5);
+      }
+    }
+  }
+}
