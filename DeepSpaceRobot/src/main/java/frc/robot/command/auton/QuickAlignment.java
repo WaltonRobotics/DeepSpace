a71d8de7fb7e613627644abe184628bfc17ce487
Rishab Rao <rishabpaws@gmail.com>
2019-03-26 18:58:59 -0400

    created quick alignment

---
+++ DeepSpaceRobot/src/main/java/frc/robot/command/auton/QuickAlignment.java
@@ -0,0 +1,38 @@
+package frc.robot.command.auton;
+
+import edu.wpi.first.wpilibj.command.Command;
+import frc.robot.OI;
+import frc.robot.Robot;
+import org.waltonrobotics.metadata.CameraData;
+
+
+public class QuickAlignment extends Command {
+
+
+  private boolean foundTarget = false;
+
+
+  @Override
+  protected void execute() {
+    CameraData currentCameraData = Robot.drivetrain.getCurrentCameraData();
+
+//    double targetAngle = Math.atan2(currentCameraData.getCameraPose().getY(), currentCameraData.getCameraPose().getX());
+//    double startAngle = currentCameraData.getCameraPose().getAngle();
+
+
+    if (currentCameraData.getNumberOfTargets() > 0) {
+      if (OI.rightJoystick.getTrigger()) {
+        SimpleAlignmentDrive.fixAlignment(currentCameraData.getCameraPose().getAngle());
+        SimpleAlignmentDrive.forwardPowerDrive(Math.abs(currentCameraData.getCameraPose().getX()));
+        foundTarget = true;
+      }
+    }
+  }
+
+  @Override
+  protected boolean isFinished() {
+    Robot.drivetrain.cancelControllerMotion();
+    return foundTarget;
+  }
+}
+
